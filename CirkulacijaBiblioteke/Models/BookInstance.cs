904b558897d93c1eb9f0059534c2c2f0767e3991
aleksaqm <perovic.aleksa03@gmail.com>
2023-07-02 20:05:30 +0200

    added models for books

---
+++ CirkulacijaBiblioteke/Models/BookInstance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirkulacijaBiblioteke.Models;
+
+public class BookInstance
+{
+    public int InventoryNumber { get; set; }
+    public float Price { get; set; }
+    public InstanceState State { get; set; }
+    public Title Title { get; set; }
+
+    public enum InstanceState
+    {
+        Available,
+        Taken,
+        Demaged
+    }
+}
---
+++ CirkulacijaBiblioteke/Models/Title.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CirkulacijaBiblioteke.Models;
+
+public class Title
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Format { get; set; }
+    public string TipKorica { get; set; }
+    public int UDK { get; set; }
+    public string ISBN { get; set; }
+    public int Godina { get; set; }
+    public List<Author> Authors { get; set; }
+    public Publisher Publisher { get; set; }
+    public List<BookInstance> BookInstances { get; set; }
+
+    [JsonConstructor]
+    public Title(string name, string description, string format, string tipKorica, int udk, string isbn, int godina, List<Author> authors, Publisher publisher, List<BookInstance> bookInstances)
+    {
+        Name = name;
+        Description = description;
+        Format = format;
+        TipKorica = tipKorica;
+        UDK = udk;
+        ISBN = isbn;
+        Godina = godina;
+        Authors = authors;
+        Publisher = publisher;
+        BookInstances = bookInstances;
+    }
+}
