fb93ffc9cdde6ca5e07e38b8b28698821c161d25
cornenkiV <vladimircornenki02@gmail.com>
2023-07-02 19:29:52 +0200

    created models for membership and membership card

---
+++ CirkulacijaBiblioteke/Models/Fine.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirkulacijaBiblioteke.Models
+{
+    public class Fine
+    {
+        public DateTime Date {  get; set; }
+        public bool Paid { get; set; }
+
+        [JsonConstructor]
+        public Fine(DateTime date, bool paid)
+        {
+            Date = date;
+            Paid = paid;
+        }
+    }
+}
